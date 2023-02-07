69165a5b47002dc8c18ddb1855216f3f4c5e67a6
northlas <rn3427@gmail.com>
2023-02-07 14:47:26 +0700

    .

--- src/main/java/com/example/happybs/HappybsApplication.java
+++ src/main/java/com/example/happybs/HappybsApplication.java
@@ -20,7 +20,7 @@ public class HappybsApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials (true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList ("http://localhost:8080/"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList ("http://localhost:8080/", "https://happy-book-store-app-ricky-nathaniel-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/"));
         corsConfiguration.setAllowedHeaders(Arrays.asList ("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With","Access-Control-Request-Method", "Access-Control-Request-Headers"));
         corsConfiguration.setExposedHeaders(Arrays.asList ("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Filename"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
